ac719a0081e44a1ee8ce73e37a5eaf107bc9bd56
Laura10257 <lyortegarobles@gmail.com>
2026-04-04 19:57:30 -0600

    infra: crear paquetes, categorias y la clase de boletos

--- SistemaBoletosEstadio/src/modelo/Boleto.java
+++ SistemaBoletosEstadio/src/modelo/Boleto.java
@@ -4,10 +4,41 @@
  */
 package modelo;
 
-/**
- *
- * @author Laura
- */
 public class Boleto {
-    
+
+    private int id;
+    private Categoria categoria;
+    private double precio;
+    private String asiento;
+    private boolean vendido;
+
+    //CONSTRUCTOR para inicializar 
+    public Boleto(int id, Categoria categoria, double precio, String asiento) {
+        this.id = id;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.asiento = asiento;
+        this.vendido = false;
+    }
+
+    //METODOS
+    public void vender() {
+        this.vendido = true;
+    }
+
+    public boolean isVendido() {
+        return vendido;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
 }
